ab58416088c42c8d7e215e9db83ff8c44460fe4f
Vernon Brighton <vernonbrighton@mail.weber.edu>
2021-08-12 20:18:55 -0700

    final

---
+++ Inventory/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Inventory
+//
+//  Created by user169317 on 8/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        PaymentMethodList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
